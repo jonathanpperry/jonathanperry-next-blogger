f2e7b6497e7ec9b214692eb886f9e3fa41329bec
Jonathan Perry <jpper@umich.edu>
2024-09-18 11:20:35 -0500

    Added links to navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,9 @@
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -20,8 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={inter.className}>
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <div>Hello world!</div>;
+  return <div>Home Page</div>;
 };
 
 export default Home;
