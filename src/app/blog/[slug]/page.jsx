cd2a7d1b49a77e619549c67640dce3bbb93378cb
Jonathan Perry <jpper@umich.edu>
2024-09-20 15:19:52 -0500

    Add post user component and use react suspense for loading state

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,21 @@
 import Image from "next/image";
 import styles from "./singlePost.module.css";
+import PostUser from "@/components/postUser/postUser";
+import { Suspense } from "react";
 
-function SinglePostPage() {
+const getData = async (slug) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
+
+const SinglePostPage = async ({ params }) => {
+  const { slug } = params;
+  const post = await getData(slug);
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
@@ -13,7 +27,7 @@ function SinglePostPage() {
         />
       </div>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>Title</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
           <Image
             className={styles.avatar}
@@ -22,19 +36,19 @@ function SinglePostPage() {
             width={50}
             height={50}
           />
-          <div className={styles.detailText}>
-            <span className={styles.detailTitle}>Author: </span>
-            <span className={styles.detailValue}>Terry Jefferson</span>
-          </div>
+          {/* User information */}
+          <Suspense fallback={<div>Loading...</div>}>
+            <PostUser userId={post.userId} />
+          </Suspense>
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published </span>
             <span className={styles.detailValue}>01.01.2024</span>
           </div>
         </div>
-        <div className={styles.content}>Lorem ipsum</div>
+        <div className={styles.content}>{post.body}</div>
       </div>
     </div>
   );
-}
+};
 
 export default SinglePostPage;
