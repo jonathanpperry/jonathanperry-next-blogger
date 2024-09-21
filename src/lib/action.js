84badddf82a25884c9e4ca486c9a5b37e20f82b7
Jonathan Perry <jpper@umich.edu>
2024-09-21 10:59:59 -0500

    Implement a couple server actions

---
+++ src/lib/action.js
@@ -0,0 +1,41 @@
+"use server";
+import { revalidatePath } from "next/cache";
+import { Post } from "./models";
+import { connectToDb } from "./utils";
+
+export const addPost = async (formData) => {
+    const { title, desc, slug, userId } = Object.fromEntries(formData);
+
+    try {
+        connectToDb();
+        const newPost = new Post({
+            title,
+            desc,
+            slug,
+            userId,
+        });
+
+        await newPost.save();
+        console.log("saved to db");
+
+        revalidatePath("/blog");
+    } catch (error) {
+        console.error(error);
+        return { error: "Something went wrong" };
+    }
+};
+
+export const deletePost = async (formData) => {
+    const { postId } = Object.fromEntries(formData);
+
+    try {
+        connectToDb();
+        await Post.findByIdAndDelete(postId);
+        console.log("deleted from db");
+
+        revalidatePath("/blog");
+    } catch (error) {
+        console.error(error);
+        return { error: "Something went wrong" };
+    }
+};
